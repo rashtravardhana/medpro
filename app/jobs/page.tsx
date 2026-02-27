e8f7fb9709089e0e5a5dba7d469dd92369655f5f
rashtravardhana <rashtravardhana123@gmail.com>
2026-02-27 18:30:53 +0530

    Create page.tsx

---
+++ app/jobs/page.tsx
@@ -0,0 +1,9 @@
+export default function JobsPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-white">
+      <h1 className="text-3xl font-semibold">
+        Browse Jobs Page
+      </h1>
+    </div>
+  );
+}
